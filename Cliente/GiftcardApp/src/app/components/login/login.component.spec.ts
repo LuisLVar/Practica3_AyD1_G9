9aa08c5fb577b4b1c90766e2e45ff03186d1e744
Gerardo cHAY <2995058550101@ingenieria.usac.edu.gt>
2020-10-14 00:09:46 -0600

    Creación de pruebas unitarias

--- Cliente/GiftcardApp/src/app/components/login/login.component.spec.ts
+++ Cliente/GiftcardApp/src/app/components/login/login.component.spec.ts
@@ -22,4 +22,29 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Testing para variables globales Login', () => {
+    it("Prueba para user, debe ser '' ", () => {
+      expect(component.user).toEqual('');
+    });
+    it("Prueba para pass, debe ser '' ", () => {
+      expect(component.pass).toEqual('');
+    });
+  });
+
+  describe('Testing a funcion resetVal()', () => {
+    it("La variable user tiene que contener '' luego de llamar a la función", () => {
+      component.user = 'Chay'
+      const auxUser  = 'Chay'
+      component.resetVal();
+      expect(auxUser).not.toBe(component.user);
+    });
+    it("La variable pass tiene que contener '' luego de llamar a la función", () => {
+      component.pass = '345Ing'
+      const auxPass  = ''
+      component.resetVal();
+      expect(auxPass ).toBe(component.user);
+    });
+  });
+
 });
--- Cliente/GiftcardApp/src/app/components/login/login.component.ts
+++ Cliente/GiftcardApp/src/app/components/login/login.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user:string = ''
+  pass:string = ''
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  resetVal(){
+    this.user=''
+    this.pass=''
+  }
+
+  ingresar(){
+    if(this.user === ''){
+      console.log("Ingrese usuario o correo","Dato requerido")
+    }
+    else if(this.pass === ''){
+      console.log("Ingrese contraseña","Dato requerido")
+    }
+    else{
+
+    }
+  }
 }
