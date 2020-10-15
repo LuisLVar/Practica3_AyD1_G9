182b485c550119988197b9ffdb4c20fd1c0ba7c5
Gerardo cHAY <2995058550101@ingenieria.usac.edu.gt>
2020-10-14 23:43:38 -0600

    Creación de compoente registro y sus modelos

---
+++ Cliente/GiftcardApp/src/app/models/usuario.ts
@@ -0,0 +1,11 @@
+export default class Usuario{
+  id:string;
+  correo:string;
+  password:string;
+  nombre:string;
+  apellido:string;
+  dpi:string;
+  edad:string;
+  tarjetas:any[];
+  transaccion:any[];
+}
