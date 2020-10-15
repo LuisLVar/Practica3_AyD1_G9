9b251756146c4edce6cb3f4f5981af19ffa55c48
Luis Vargas <luisvarleon12@gmail.com>
2020-10-15 16:21:21 -0600

    Componentes y servicios de administrador

--- Cliente/GiftcardApp/src/app/app.module.ts
+++ Cliente/GiftcardApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdministradorComponent } from './components/administrador/administrador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministradorComponent
   ],
   imports: [
     BrowserModule,
