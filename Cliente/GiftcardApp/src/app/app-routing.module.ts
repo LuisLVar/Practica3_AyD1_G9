39cb5338874f4a7b7bde32bb4b5efc7dd17ee17d
Luis Vargas <luisvarleon12@gmail.com>
2020-10-17 22:53:47 -0600

    Prueba satisfactoria para tarifa

--- Cliente/GiftcardApp/src/app/app-routing.module.ts
+++ Cliente/GiftcardApp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdministradorComponent } from './components/administrador/administrador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/portada', pathMatch: 'full' },
+  { path: 'admin', component:  AdministradorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
