5209ae00d68bc898dbabbc41708b5fccb312aa30
Luis Vargas <luisvarleon12@gmail.com>
2020-10-30 22:35:11 -0600

    [FIX] Transaccion server

--- Servidor/build/routes/apiRoutes.js
+++ Servidor/build/routes/apiRoutes.js
@@ -9,16 +9,13 @@ class ApiRoutes {
         this.config();
     }
     config() {
-<<<<<<< HEAD
-        this.router.get('/aux/cards', apiController_1.apiController.getCardsAux);
+        //this.router.get('/aux/cards', apiController.getCardsAux);
         this.router.get('/values', adminController_1.adminController.getValues);
         this.router.get('/giftcard', adminController_1.adminController.getGiftCards);
         this.router.post('/giftcard/regalo', adminController_1.adminController.regalarGiftcard);
         this.router.get('/transaccion', adminController_1.adminController.getTransacciones);
-=======
         this.router.get('/aux/sync', apiController_1.apiController.sync);
         this.router.get('/aux/tasa', apiController_1.apiController.tasa);
->>>>>>> 4027019d8e2e5a316777d3df2e4edd3a7682eef4
     }
 }
 const apiRoutes = new ApiRoutes();
