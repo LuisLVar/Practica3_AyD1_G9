2fad256e4ba0a9f7d0c08fee44da810c6d762b21
Feliciano07 <fernandoxhajon122@gmail.com>
2020-11-02 15:56:14 -0600

    Pruebas e2e finalizadas en login

--- Cliente/GiftcardApp/e2e/src/app.e2e-spec.ts
+++ Cliente/GiftcardApp/e2e/src/app.e2e-spec.ts
@@ -1,17 +1,35 @@
-import { AppPage } from './app.po';
+import { AppPage, Login } from './app.po';
 import { browser, logging } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
+  let _login: Login
 
   beforeEach(() => {
     page = new AppPage();
+    _login = new Login();
   });
 
-  it('should display welcome message', () => {
+  /* it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('GiftcardApp app is running!');
-  });
+  }); */
+
+  it("Inicio de sesion satisfactorio" ,() =>{
+    _login.navigateTo();
+    _login.setValuesLogin("feliciano07", "123456");
+    expect(_login.inputEmail.getAttribute('value')).toEqual('feliciano07');
+    expect(_login.inputPassword.getAttribute('value')).toEqual('123456')
+    _login.btnSesion.click();
+  })
+
+  it("Inicio de sesion fallado", () => {
+    _login.navigateTo();
+    _login.setValuesLogin("feliciano07", "123456");
+    expect(_login.inputEmail.getAttribute('value')).toEqual('felix');
+    expect(_login.inputPassword.getAttribute('value')).toEqual('123456')
+    _login.btnSesion.click();
+  })
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- Cliente/GiftcardApp/e2e/src/app.po.ts
+++ Cliente/GiftcardApp/e2e/src/app.po.ts
@@ -11,3 +11,25 @@ export class AppPage {
 }
 
 
+export class Login{
+  inputEmail: ElementFinder;
+  inputPassword: ElementFinder;
+  btnSesion: ElementFinder;
+
+  constructor(){
+    this.inputEmail = element(by.id('inputEmail'));
+    this.inputPassword = element(by.id('inputPassword'));
+    this.btnSesion = element(by.id('btnSesion'));
+  }
+
+  // move to login
+  async navigateTo(){
+    await browser.get(`${browser.baseUrl}login`);
+  }
+
+  async setValuesLogin(email: string, pass: string){
+    await this.inputEmail.sendKeys(email);
+    await this.inputPassword.sendKeys(pass);
+  }
+
+}
